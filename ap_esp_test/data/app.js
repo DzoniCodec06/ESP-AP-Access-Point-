c809ecb165b7096ceb80645ded16e7ec3db008d8
Nikola Bojinović <bojinovicnikola12@gmail.com>
2025-08-15 12:10:07 +0200

    Add files via upload

--- ap_esp_test/data/app.js
+++ ap_esp_test/data/app.js
@@ -1,27 +1,14 @@
-/*
-const sbmBtn = document.getElementById("sbmBtn");
-const porukaEl = document.getElementById("poruka");
-const ponavljanjeEl = document.getElementById("ponavljanje");
+const textBtn = document.getElementById("txtBtn");
+const solidBtn = document.getElementById("solidBtn");
 
-sbmBtn.addEventListener("submit", e => {
-    e.preventDefault();
-    console.log("cao");
-    porukaEl.value = "";
-    ponavljanjeEl.value = false;
-})*/
-/*
-const formEl = document.getElementById("forma");
-    
-formEl.addEventListener("submit", e => {
-    e.preventDefault();
+const container = document.getElementById("cont");
 
-    const formData = new FormData(e.currentTarget);
-    const params = new URLSearchParams(formData).toString();
-        
-    fetch(`/submit?${params}`)
-        .then(response => {
-        // Immediately redirect to "/"
-        window.location.reload();
-    })
+textBtn.addEventListener("click", () => {
+    textBtn.classList.replace("uab", "ab");
+    solidBtn.classList.replace("ab", "uab");
 });
-*/
+
+solidBtn.addEventListener("click", () => {
+    textBtn.classList.replace("ab", "uab");
+    solidBtn.classList.replace("uab", "ab");
+});
